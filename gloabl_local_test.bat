7f640e514651815bf1f8791f264591d7b318027c
unknown <qzane@live.com>
2016-12-31 10:42:01 +0800

    add windows test files

---
+++ gloabl_local_test.bat
@@ -0,0 +1,6 @@
+type Prime.java
+.\miniJava.exe -r Prime.java
+@echo off
+echo The function 'test' tried to print out 'i' and 'count',
+echo but 'i' is a local variable of 'main', so it failed.
+echo However, 'count' is a global variable, so it succeeded.
